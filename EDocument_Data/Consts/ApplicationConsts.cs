d74740abdee4f7be6fef760206ae2fe5ee074eb3
Alaa Abo Elhassan <alaa.muhammad@dpworld.com>
2023-09-15 10:27:03 +0300

    Update environment variables with user instead of system

--- EDocument_Data/Consts/ApplicationConsts.cs
+++ EDocument_Data/Consts/ApplicationConsts.cs
@@ -8,8 +8,8 @@ namespace EDocument_Data.Consts
 {
     public abstract class ApplicationConsts
     {
-        public static string? ConnectionString { get; private set; } = Environment.GetEnvironmentVariable("eDoc", EnvironmentVariableTarget.Machine);
-        public static string? SecretKey { get; private set; } = Environment.GetEnvironmentVariable("eDoc_SK", EnvironmentVariableTarget.Machine);
+        public static string? ConnectionString { get; private set; } = Environment.GetEnvironmentVariable("E-Doc", EnvironmentVariableTarget.User);
+        public static string? SecretKey { get; private set; } = Environment.GetEnvironmentVariable("E-Doc SK", EnvironmentVariableTarget.User);
         public static string? EmailPassword { get; private set; } = Environment.GetEnvironmentVariable("EP", EnvironmentVariableTarget.User);
         public static string ClientOrigin { get; private set; } = "https://localhost:4200";
         public static string ADPath { get; private set; } = "LDAP://spdc.com";
