3d9a8faa246ee553d2ffcf29c9f3606d4c64e63f
Alaa Abo Elhassan <alaa.muhammad@dpworld.com>
2023-11-21 09:21:52 +0200

    prevent automatic approve for the creator should be prevented for Refund

--- EDocument_Reposatories/Application Repositories/Request Reviewer Repository/RequestReviewerRepository.cs
+++ EDocument_Reposatories/Application Repositories/Request Reviewer Repository/RequestReviewerRepository.cs
@@ -160,7 +160,7 @@ namespace EDocument_Repositories.Application_Repositories.Request_Reviewer_Repos
                 }
 
                 // Skip All Approval steps for the creator if he is one of the reviewers
-                if (request.RequestReviewers.Any(r => r.AssignedReviewerId == request.CreatorId) && definedRequestId!= 2023110902543453 && definedRequestId!= 2023110902550379) //Automatic approve for the creator should be prevented for New Item and Pr Requests as store team need to insert data in approve
+                if (request.RequestReviewers.Any(r => r.AssignedReviewerId == request.CreatorId) && definedRequestId!= 2023110902543453 && definedRequestId!= 2023110902550379 && definedRequestId != 2023101010522512) //Automatic approve for the creator should be prevented for Refund and  New Item and Pr Requests as store team need to insert data in approve
                 {
                     var reviewerStageNumber = request.RequestReviewers.FirstOrDefault(rr=>rr.AssignedReviewerId == request.CreatorId).StageNumber;
                     foreach (var reviewer in request.RequestReviewers.Where(r => r.StageNumber == reviewerStageNumber))
