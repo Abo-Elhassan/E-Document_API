d580d6c743e9689ef13bdcb3661bc6d9ba5335b5
Alaa Abo Elhassan <alaa.muhammad@dpworld.com>
2023-11-21 15:04:39 +0200

    add concerned department column in refund request

---
+++ EDocument_EF/Migrations/20231121125111_AddConcernedDepartmentRefundRequest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EDocument_EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConcernedDepartmentRefundRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConcernedDepartment",
+                table: "RefundRequest",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConcernedDepartment",
+                schema: "audit",
+                table: "AuditRefundRequest",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConcernedDepartment",
+                table: "RefundRequest");
+
+            migrationBuilder.DropColumn(
+                name: "ConcernedDepartment",
+                schema: "audit",
+                table: "AuditRefundRequest");
+        }
+    }
+}
