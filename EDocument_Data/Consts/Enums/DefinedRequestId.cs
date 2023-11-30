a443935618c45f38715062ce12bc07a4d3384007
Alaa Abo Elhassan <alaa.muhammad@dpworld.com>
2023-11-30 09:03:26 +0200

    prevent automatic update for specific Requests

---
+++ EDocument_Data/Consts/Enums/DefinedRequestId.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDocument_Data.Consts.Enums
+{
+    public abstract class DefinedRequestId
+    {
+       
+        public static long Po { get; private set; } = 2023101010207127;
+        public static long Vehicle { get; private set; } = 2023101010307855;
+        public static long Travel_Desk { get; private set; } = 2023101010407085;
+        public static long Refund { get; private set; } = 2023101010522512;
+        public static long CCTV_Access { get; private set; } = 2023110110291740;
+        public static long Access_Control { get; private set; } = 2023110204192566;
+        public static long Fuel_Oil_Invoice { get; private set; } = 2023110902494107;
+        public static long New_Item { get; private set; } = 2023110902543453;
+        public static long Pr { get; private set; } = 2023110902550379;
+        public static long Manlift_Reservation { get; private set; } = 2023110902555103;
+        public static long Equipment_Request_Inside { get; private set; } = 2023111310582624;
+        public static long Equipment_Request_Outside { get; private set; } = 2023111311113090;
+        public static long Reschedule_PM_WO { get; private set; } = 2023112111535000;
+        public static long PM_JP { get; private set; } = 2023112307532308;
+        public static long Multimedia { get; private set; } = 2023112709173645;
+        public static long Voucher { get; private set; } = 2023112802152106;
+        public static long Discount { get; private set; } = 2023112811460600;
+        
+
+        
+
+    }
+}
