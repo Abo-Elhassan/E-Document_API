13d4c9fecf812588fc7b668d98c3eb201fe5db09
Alaa Abo Elhassan <alaa.muhammad@dpworld.com>
2023-11-30 09:03:55 +0200

    Validate approval attachment extentions

--- EDocument_Data/DTOs/Requests/DiscountRequest/ApproveMultimediaRequestDto.cs
+++ EDocument_Data/DTOs/Requests/DiscountRequest/ApproveMultimediaRequestDto.cs
@@ -1,4 +1,5 @@
 ﻿
+using EDocument_Data.Custom_Attributes;
 using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations;
 
@@ -10,6 +11,7 @@ namespace EDocument_Data.DTOs.Requests.DiscountRequest
         [Required]
         public long RequestId { get; init; }
         public float? DiscountAmount { get; init; }
+        [FileValidation(100 * 1024 * 1024, true, ".pdf", ".docx", ".xlsx", ".xls", ".csv", ".zip", ".png", ".jpg", ".jpeg")]
         public IFormFile? HoSupportedDocument { get; init; }
         public string? ReviewedNotes { get; init; }
     }
--- EDocument_Data/DTOs/Requests/MultimediaRequest/ApproveMultimediaRequestDto.cs
+++ EDocument_Data/DTOs/Requests/MultimediaRequest/ApproveMultimediaRequestDto.cs
@@ -1,4 +1,5 @@
 ﻿
+using EDocument_Data.Custom_Attributes;
 using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations;
 
@@ -10,6 +11,7 @@ namespace EDocument_Data.DTOs.Requests.MultimediaRequest
         [Required]
         public long RequestId { get; init; }
         public DateTime? EstimatedDeliveryDate { get; init; }
+        [FileValidation(100 * 1024 * 1024, true, ".pdf", ".docx", ".xlsx", ".xls", ".csv", ".zip", ".png", ".jpg", ".jpeg")]
         public IFormFile? PrTeamAttachment { get; init; }
         public string? ReviewedNotes { get; init; }
     }
