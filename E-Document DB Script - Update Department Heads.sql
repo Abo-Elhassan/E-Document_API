86be0357580fddb9fe76b05cfb1d170f93265040
Abo-Elhassan <107323255+Abo-Elhassan@users.noreply.github.com>
2024-04-25 15:34:58 +0200

    Delete E-Document DB Script - Update Department Heads.sql

--- E-Document DB Script - Update Department Heads.sql
+++
@@ -1,4 +0,0 @@
-UPDATE Department SET ManagerId = '51688' WHERE Id = 6 --make hend head of communications
-UPDATE Department SET ManagerId = '50439' WHERE Id = 20 --make hany hamdy head of stores
-UPDATE Department SET ManagerId = '50439' WHERE Id = 32 --make hany hamdy head of warehouse
-UPDATE Department SET ManagerId = '50962' WHERE Id = 31 --make mohamed tarek head of Governamental affairs
